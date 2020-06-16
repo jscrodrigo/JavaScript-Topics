6ae89bbe70dfafc350ac6771daa7f593199973ce
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-06-16 08:33:45 -0300

    Basic average calculation

---
+++ index.js
@@ -0,0 +1,12 @@
+const student1 = 'Amanda';
+const student2 = 'Billy';
+const student3 = 'Rooney';
+
+const salaryStudent1 = 2550;
+const salaryStudent2 = 4000;
+const salaryStudent3 = 1500;
+
+const avg = (salaryStudent1 + salaryStudent2 + salaryStudent3) / 3;
+
+console.log(`${student1} has a salary of ${salaryStudent1}. ${student2} has a salaray of
+${salaryStudent2} and ${student3} has a salary of ${salaryStudent3}`);
