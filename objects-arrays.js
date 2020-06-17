0bfe5a7ef330f47424c653e192add5f24428e066
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-06-17 08:09:45 -0300

    code example of objects and arrays improved.

--- objects-arrays.js
+++ objects-arrays.js
@@ -1,10 +1,34 @@
-const student01 = {
-  name: "Francis",
-  age: 18,
-  course: "Psycology"
-}
-const student02 = {
-  name: "Ruth",
-  age: 21,
-  course: "Computer Science"
-}
+/*******Objects********/
+// const student01 = {
+//   name: "Francis",
+//   age: 18,
+//   course: "Psycology"
+// }
+// const student02 = {
+//   name: "Ruth",
+//   age: 21,
+//   course: "Computer Science"
+// }
+// console.log(student01.course);
+// console.log(student02.course);
+
+/*Arrays*/
+const courses = ["Math", "Chemestry", "Automation Engineering", "Medicine"];
+console.log(courses[0]);
+
+const client = [
+  {
+    name: "Patrick",
+    bill: 55
+  },
+  {
+    name: "Anna",
+    bill: 78
+  },
+  {
+    name: "Benjamin",
+    bill: 155
+  }
+]
+
+console.log(client[2].name);
